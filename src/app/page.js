41f20fe184a331275c0654187f0dd63d7f91d902
Muntasir333 <mdfahimmuntasir333@gmail.com>
2026-06-29 04:49:58 +0600

    3rd commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,12 @@ import Banner from "@/components/Banner";
 import FeaturedSection from "@/components/Featured";
 import ContactUs from "@/components/Contact";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <Banner />
       <FeaturedSection />
       <ContactUs />
